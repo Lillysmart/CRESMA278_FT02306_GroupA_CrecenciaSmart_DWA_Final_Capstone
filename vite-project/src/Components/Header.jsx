7457e3eafc2d7355d682728dffc83762213b0a8c
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-11-17 14:06:09 +0200

    run a fetch to show the data on the screen

--- vite-project/src/Components/Header.jsx
+++ vite-project/src/Components/Header.jsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
 
 export const Header = () => {
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     fetch("https://podcast-api.netlify.app/id/10716")
       .then((response) => {
         return response.json();
       })
-      .then(console.log("Hi"));
+      .then((responseData)=>{
+        console.log("Fetched data:", responseData);
+        setData(responseData)
+      })
+
   }, []);
+
+
+ 
 
   return (
     <div>
-      <h1> Hello there</h1>
+      <h1> {data}</h1>
     </div>
   );
 };
