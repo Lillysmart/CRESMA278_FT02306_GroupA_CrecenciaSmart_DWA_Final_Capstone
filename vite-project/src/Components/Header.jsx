faad174d277805db2c5eaf15b28469f280229c05
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-11-17 13:51:51 +0200

    Update Header.jsx

--- vite-project/src/Components/Header.jsx
+++ vite-project/src/Components/Header.jsx
@@ -1,10 +1,17 @@
-import React ,{useState , useEffect} from "react"
+import React, { useState, useEffect } from "react";
 
- export const Header =()=>{
-    return (
-        <div>
-            <h1> Hello there</h1>
-        </div>
-    )
-}
+export const Header = () => {
+  useEffect(() => {
+    fetch("https://podcast-api.netlify.app/id/10716")
+      .then((response) => {
+        return response.json();
+      })
+      .then(console.log("Hi"));
+  }, []);
 
+  return (
+    <div>
+      <h1> Hello there</h1>
+    </div>
+  );
+};
