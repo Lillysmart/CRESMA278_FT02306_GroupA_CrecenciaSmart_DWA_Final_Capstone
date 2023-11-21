ad833bc689677173710a0738661f5c76451f0b11
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-11-21 12:14:29 +0200

    Update ShowDetails.jsx

--- vite-project/src/Components/ShowDetails.jsx
+++ vite-project/src/Components/ShowDetails.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 // ... (import statements)
@@ -39,67 +38,82 @@ export const ShowDetail = () => {
     setSelectedSeason(event.target.value);
   };
 
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
-    <div className="show-container">
-      {showDetails && (
-        <div className="show-card">
-          <button className="back-button" onClick={handleBackClick}>
-            Back to Preview
-          </button>
-          <h1 className="show-title">{showDetails.title}</h1>
-          <img className="show-image" src={showDetails.image} alt={showDetails.title} />
-          <p className="show-description">{showDetails.description}</p>
-
-          {/* Styled Season Selection */}
-          <div className="season-selection">
-            <label htmlFor="seasonSelect" className="season-label"> Season:</label>
-            <select
-              id="seasonSelect"
-              onChange={handleSeasonSelect}
-              value={selectedSeason || ''}
-              className="season-select"
-            >
-              <option value="" disabled>Select a season</option>
-              {showDetails.seasons.map((season) => (
-                <option key={season.season} value={season.season}>
-                  {season.season}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          {/* Display selected season information */}
-          {selectedSeason && (
-            <div className="selected-season-info">
-              <h2>{`Season ${selectedSeason}`}</h2>
-              {showDetails.seasons.map((season) => {
-                if (season.season.toString() === selectedSeason) {
-                  return (
-                    <div key={season.season} className="season-container">
-                      
-                      {season.episodes && season.episodes.length > 0 ? (
-                        season.episodes.map((episode) => (
-                          <div key={episode.episode} className="episode-card">
-                            <h4>Episode {episode.episode} : {episode.title}</h4>
-                            <p>{episode.description}</p>
-                            <audio controls>
-                              <source src={episode.file} type="audio/mp3" />
-                              Your browser does not support the audio element.
-                            </audio>
-                          </div>
-                        ))
-                      ) : (
-                        <p>No episodes available for this season.</p>
-                      )}
-                    </div>
-                  );
-                }
-                return null;
-              })}
+    <>
+      <div className="fixed-buttons">
+        <button className="back-button" onClick={handleBackClick}>
+          Back to Preview
+        </button>
+        <button>Hello</button>
+      </div>
+      <div className="show-container">
+        {showDetails && (
+          <div className="show-card">
+            <div className="show-top-info">
+              {/* ... Top info content ... */}
             </div>
-          )}
-        </div>
-      )}
-    </div>
+            <h1 className="show-title">{showDetails.title}</h1>
+            <img className="show-image" src={showDetails.image} alt={showDetails.title} />
+            <p className="show-description">{showDetails.description}</p>
+
+            {/* Styled Season Selection */}
+            <div className="season-selection">
+              <label htmlFor="seasonSelect" className="season-label"> Season:</label>
+              <select
+                id="seasonSelect"
+                onChange={handleSeasonSelect}
+                value={selectedSeason || ''}
+                className="season-select"
+              >
+                <option value="" disabled>Select a season</option>
+                {showDetails.seasons.map((season) => (
+                  <option key={season.season} value={season.season}>
+                    {season.season}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            {/* Display selected season information */}
+            {selectedSeason && (
+              <div className="selected-season-info">
+                <h2>{`Season ${selectedSeason}`}</h2>
+                {showDetails.seasons.map((season) => {
+                  if (season.season.toString() === selectedSeason) {
+                    return (
+                      <div key={season.season} className="season-container">
+                        {season.episodes && season.episodes.length > 0 ? (
+                          season.episodes.map((episode) => (
+                            <div key={episode.episode} className="episode-card">
+                              <h4>Episode {episode.episode} : {episode.title}</h4>
+                              <p>{episode.description}</p>
+                              <audio controls>
+                                <source src={episode.file} type="audio/mp3" />
+                                Your browser does not support the audio element.
+                              </audio>
+                            </div>
+                          ))
+                        ) : (
+                          <p>No episodes available for this season.</p>
+                        )}
+                      </div>
+                    );
+                  }
+                  return null;
+                })}
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </>
   );
 };
