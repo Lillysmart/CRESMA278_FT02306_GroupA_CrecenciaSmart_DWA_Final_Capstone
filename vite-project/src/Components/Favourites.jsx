824c4f704b001a3d9ae9536c832bccb2c40c58c9
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-11-24 10:18:35 +0200

    Add styling for the favourite card

--- vite-project/src/Components/Favourites.jsx
+++ vite-project/src/Components/Favourites.jsx
@@ -1,6 +1,6 @@
 // Favourites.jsx
-import React from 'react';
-import { useFavoritesContext } from './FavoritesContext';
+import React from "react";
+import { useFavoritesContext } from "./FavoritesContext";
 
 const Favourites = () => {
   const { favorites, removeFromFavorites } = useFavoritesContext();
@@ -8,33 +8,39 @@ const Favourites = () => {
   const handleRemoveFromFavorites = (episodeId) => {
     removeFromFavorites(episodeId);
   };
-  const date = new Date()
-  const todayDate= `${date.getDate()}/${date.getMonth()+1}/ ${date.getFullYear()}`
+  const date = new Date();
+  const todayDate = `${date.getDate()}/${
+    date.getMonth() + 1
+  }/ ${date.getFullYear()}`;
 
   return (
     <div>
       <h1>Your Favorites :</h1>
       {favorites.length > 0 ? (
-        <ul className='favourite-list'>
+        <ul className="favourite-list">
           {favorites.map(({ episode, show, season }) => (
             <li key={episode.episode}>
-              <div className='favourite-shows'>
+              <div className="favourite-shows">
                 <h3>{`${show}`}</h3>
                 <h4>{`Season: ${season} `}</h4>
-                <p>{`Episode ${episode.episode}: ${episode.title}`}</p>
-                <audio controls>
-                                <source src={episode.file} type="audio/mp3" />
-                                Your browser does not support the audio element.
-                              </audio>
-                              <br />
-                              <p>Date Added:{todayDate}</p>
-                              <button onClick={() => handleRemoveFromFavorites(episode.episode)}>
+                <p>{`Episode ${episode.episode}: ${episode.title.slice(0, 25)}${
+                  episode.title.length > 25? "..." : ""
+                }`}</p>
+                  <div className="audio-container">
+                  <audio controls style={{ maxWidth: '100%' }}>
+                    <source src={episode.file} type="audio/mp3" />
+                    Your browser does not support the audio element.
+                  </audio>
+                </div>
+                <br />
+                <p>Date Added:{todayDate}</p>
+                <button
+                  onClick={() => handleRemoveFromFavorites(episode.episode)}
+                >
                   Remove from Favorites
                 </button>
               </div>
-              <div>
-              
-              </div>
+              <div></div>
             </li>
           ))}
         </ul>
