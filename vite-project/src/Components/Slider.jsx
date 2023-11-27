e45da4c29206f5c9de9e9c4e015f5d822289292b
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-11-27 18:26:41 +0200

    Add styling to the slider

--- vite-project/src/Components/Slider.jsx
+++ vite-project/src/Components/Slider.jsx
@@ -1,13 +1,31 @@
-import React from "react"
 
-const SliderComponent = ({ items }) => {
+import React , {useState}from "react"
+
+export const Slider= ({ shows }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
   
     const nextSlide = () => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % items.length);
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % shows.length);
     };
   
     const prevSlide = () => {
-      setCurrentIndex((prevIndex) => (prevIndex - 1 + items.length) % items.length);
+      setCurrentIndex((prevIndex) => (prevIndex - 1 + shows.length) % shows.length);
     };
-    return ()}
+
+
+    return ( <div className="sliding-carousel-container">
+    <div className="sliding-carousel-track" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+      {shows.map((show, index) => (
+        <div key={index} className="sliding-carousel-item">
+          {show}
+        </div>
+      ))}
+    </div>
+    <button className="sliding-carousel-button prev" onClick={prevSlide}>
+    </button>
+    <button className="sliding-carousel-button next" onClick={nextSlide}>
+      ❯
+    </button>
+  </div>
+)}
+ export default Slider
